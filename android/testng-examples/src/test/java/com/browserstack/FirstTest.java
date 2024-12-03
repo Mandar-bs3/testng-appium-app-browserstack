cb70fa3d64048ece5236cabd340f3b0fd9bf0cef
Mandar-bs3 <mandar@browserstack.com>
2024-12-03 14:48:22 +0530

    script added for few more appium command.

--- android/testng-examples/src/test/java/com/browserstack/FirstTest.java
+++ android/testng-examples/src/test/java/com/browserstack/FirstTest.java
@@ -1,7 +1,7 @@
 package com.browserstack;
 
 import java.time.Duration;
-import java.util.List;
+import java.util.*;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -15,16 +15,30 @@ import java.io.File;
 import java.net.URL;
 import java.nio.file.Paths;
 import org.openqa.selenium.ScreenOrientation;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.MultiTouchAction;
+import io.appium.java_client.android.AndroidTouchAction;
+import io.appium.java_client.touch.offset.PointOption;
+//import io.appium.java_client.android.AndroidKeyCode;
+import org.openqa.selenium.JavascriptExecutor;
 
 public class FirstTest extends AppiumTest {
 
     @Test
     public void test() throws Exception {
       driver.launchApp();
+      WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
       driver.setLocation(new Location(49, 123, 10));
       WebElement continueButton = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30)).until(
                 ExpectedConditions.elementToBeClickable(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")));
       continueButton.click();
+      driver.performTouchAction(new TouchAction(driver).tap(PointOption.point(500, 500)));
+      TouchAction action1 = new TouchAction(driver).press(PointOption.point(500, 500));
+      TouchAction action2 = new TouchAction(driver).press(PointOption.point(700, 500));
+
+      MultiTouchAction multiTouchAction = new MultiTouchAction(driver);
+      driver.performMultiTouchAction(multiTouchAction.add(action1).add(action2));
 
       WebElement skipButton = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30)).until(
              ExpectedConditions.elementToBeClickable(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")));
@@ -36,7 +50,10 @@ public class FirstTest extends AppiumTest {
       searchElement.click();
       WebElement insertTextElement = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30)).until(
           ExpectedConditions.elementToBeClickable(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")));
+      insertTextElement.click();
+      driver.hideKeyboard();
       insertTextElement.sendKeys("test");
+//      driver.pressKeyCode(AndroidKeyCode.SPACE);
       insertTextElement.clear();
       insertTextElement.sendKeys("BrowserStack");
 
@@ -50,18 +67,26 @@ public class FirstTest extends AppiumTest {
       searchResultItem.click();
       Thread.sleep(5000);
       driver.rotate(ScreenOrientation.LANDSCAPE);
+//      driver.longPressKeyCode(AndroidKeyCode.HOME);
       driver.resetApp();
-      String relativePath = "WikipediaSample.apk";  // Relative path to APK file
+      String relativePath = "EspressoTestingUrl-debug.apk";  // Relative path to APK file
       String projectDir = System.getProperty("user.dir");  // Get the current project directory
       File appFile = Paths.get(projectDir, relativePath).toFile();  // Resolve the absolute path
-      String bundleId = "org.wikipedia.alpha";
-      driver.terminateApp(bundleId);
-      driver.activateApp(bundleId);
+      String bundleId = "com.bsstag.espressotesting";
       String appActivity = "org.wikipedia.main.MainActivity";
 //      driver.startActivity("appPackage",bundleId, null, null);
       if (appFile.exists()) {
-        System.out.println("Found APK: " + appFile.getAbsolutePath());
-//        driver.installApp(appFile.getAbsolutePath());
+        driver.installApp("https://qa-live-server.bsstag.com/download/EspressoTestingUrl-debug.apk");
       }
+      Set<String> contextNames = driver.getContextHandles();
+      System.out.println("Available Contexts:");
+      for (String contextName : contextNames) {
+        System.out.println(contextName);
+      }
+      driver.activateApp(bundleId);
+      driver.context(contextNames.toArray(new String[contextNames.size()])[0]);
+      driver.terminateApp(bundleId);
+//      JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+//      jsExecutor.executeScript("document.body.style.backgroundColor = 'lightblue';");
     }
 }
