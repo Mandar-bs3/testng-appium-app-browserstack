9b1093ccd0a6d8edf6d873f54ff604ede13d206a
Mandar-bs3 <mandar@browserstack.com>
2024-12-05 14:06:33 +0530

    ios commands added.

--- ios/testng-examples/src/test/java/com/browserstack/FirstTest.java
+++ ios/testng-examples/src/test/java/com/browserstack/FirstTest.java
@@ -5,16 +5,24 @@ import org.testng.annotations.Test;
 
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
+import org.openqa.selenium.html5.Location;
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.WebElement;
-
+import org.openqa.selenium.ScreenOrientation;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.MultiTouchAction;
+import io.appium.java_client.android.AndroidTouchAction;
+import io.appium.java_client.touch.offset.PointOption;
 import java.time.Duration;
+import java.util.Map;
 
 public class FirstTest extends AppiumTest {
 
   @Test
   public void test() throws Exception {
+    driver.launchApp();
+    driver.setLocation(new Location(49, 123, 10));
+
 
     new WebDriverWait(driver, Duration.ofSeconds(30)).until(
             ExpectedConditions.elementToBeClickable(AppiumBy.className("XCUIElementTypeStaticText")));
@@ -24,10 +32,19 @@ public class FirstTest extends AppiumTest {
     newButton.click();
     Thread.sleep(50);
 
+    driver.performTouchAction(new TouchAction(driver).tap(PointOption.point(500, 500)));
+    TouchAction action1 = new TouchAction(driver).press(PointOption.point(500, 500));
+    TouchAction action2 = new TouchAction(driver).longPress(PointOption.point(700, 500));
+    TouchAction action3 = new TouchAction(driver).moveTo(PointOption.point(800, 800));
+
+    MultiTouchAction multiTouchAction = new MultiTouchAction(driver);
+    driver.performMultiTouchAction(multiTouchAction.add(action1).add(action2).add(action3));
+
     WebElement bestButton = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30)).until(
         ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Best")));
     bestButton.click();
     Thread.sleep(50);
+    driver.rotate(ScreenOrientation.LANDSCAPE);
 
     WebElement favButton = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30)).until(
             ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Fav")));
@@ -39,5 +56,10 @@ public class FirstTest extends AppiumTest {
     settingsButton.click();
     Thread.sleep(50);
     driver.shake();
+    driver.resetApp();
+    driver.activateApp("japan.tokyo.chiyoda.sogarage.ios.HackerNewsScrapBookFree");
+    driver.rotate(ScreenOrientation.PORTRAIT);
+    Map<String, Object> deviceInfo = (Map<String, Object>) driver.executeScript("mobile: deviceInfo");
+    System.out.println("Device Model: " + deviceInfo.get("model"));
   }
 }
